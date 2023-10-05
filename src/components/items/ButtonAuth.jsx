081354ee5a189f834df1d9482ca023a767dc7da2
Nayandra Kastoro <kastoronayandra@gmail.com>
2023-10-05 21:01:57 +0700

    selesai membuat halaman login dan register

---
+++ src/components/items/ButtonAuth.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+export const ButtonAuth = ({ text }) => {
+  return (
+    <button className="w-full bg-[#FFCC81] py-3 rounded-full text-[#3C424C] text-lg font-bold cursor-pointer">
+      {text}
+    </button>
+  );
+};
---
+++ src/page/Signup.jsx
@@ -0,0 +1,61 @@
+import { ButtonAuth } from "../components/items/ButtonAuth";
+
+export const SignUp = () => {
+  return (
+    <div className="w-full h-5/6 flex justify-center items-center">
+      <div className="w-[400px] mx-auto ">
+        <img src="assets/image/logo2.png" alt="" className="block mx-auto" />
+        <h1 className="text-[#3C424C] text-2xl font-bold text-center mt-5">
+          Sign up for Meowing
+        </h1>
+        <form className="mt-9">
+          <div className="w-full mb-8">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="w-full bg-transparent outline-none border-b-2 pb-2 border-[#3C424C] placeholder:text-[#3C424C] text-slate-600"
+              autoComplete="off"
+            />
+          </div>
+          <div className="w-full mb-8">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              className="w-full bg-transparent outline-none border-b-2 pb-2 border-[#3C424C] placeholder:text-[#3C424C]"
+              autoComplete="off"
+            />
+          </div>
+          <div className="w-full mb-8">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="w-full bg-transparent outline-none border-b-2 pb-2 border-[#3C424C] placeholder:text-[#3C424C]"
+              autoComplete="off"
+            />
+          </div>
+          <div className="w-full mb-8">
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              className="w-full bg-transparent outline-none border-b-2 pb-2 border-[#3C424C] placeholder:text-[#3C424C]"
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex gap-2 mb-5">
+            <input type="checkbox" className="accent-slate-600" />
+            <p className="text-sm">Agree to join Lorem Ipsum Meowing</p>
+          </div>
+          <ButtonAuth text={"Create Account"} />
+        </form>
+        <p className="mt-16 text-[#3C424C] text-sm text-center">
+          Already have an account<span className="font-semibold">?</span>{" "}
+          <span className="font-bold">Sign In</span>
+        </p>
+      </div>
+    </div>
+  );
+};
