94ef8df880ca537d93583ac1414d8fd256bfd7ff
Rifky <rifkyferdiansyah211@gmail.com>
2023-10-12 02:31:39 +0700

    update community page

--- src/components/items/Comment.jsx
+++ src/components/items/Comment.jsx
@@ -1,7 +1,10 @@
-export const CommentContain = () => {
+import { AiFillLike } from 'react-icons/ai';
+
+export const CommentContent = () => {
   return (
     <>
-      <div className="my-3">
+      {/* foto profile */}
+      <div className="my-3 flex-1">
         <div className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[834px] h-[191px] flex justify-between items-center">
           <img
             src="http://placehold.it/47x47"
@@ -9,18 +12,30 @@ export const CommentContain = () => {
             className="rounded-full ml-8 mb-[100px]"
           />
 
-          <div className="mx-8">
-            <div className="comment pb-[65px]">
-              <h3 className="font-bold text-[20px]">
+          {/* kolom komen */}
+          <div className="mx-8 flex-col">
+            <div className="comment pt-[35px]">
+              <h3 className="font-bold text-[20px] text-justify">
                 Halo, Guys aku mau tanya terkait perawatan kucing yang baik itu
                 gimana ya. Aku baru aja adopsi kucing
               </h3>
-              <div className="flex items-center space-x-3 pt-[5px]">
-                <p className="pt-[px] text-[14px] text-[#FFCC81] font-bold">
-                  Iqbal Arieftian
-                </p>
-                <p className="font-thin text-[14px] text-gray-400">Baru Saja</p>
+            </div>
+
+            <div className="flex items-center space-x-3 pt-[5px] pb-[18px]">
+              <p className="pt-[px] text-[14px] text-[#FFCC81] font-bold">
+                Iqbal Arieftian
+              </p>
+              <p className="font-thin text-[14px] text-gray-400">Baru Saja</p>
+            </div>
+
+            <div className="pt-[5px] pb-[40px] flex items-center">
+              <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#D9D9D9] bg-opacity-[42%] rounded-full">
+                <AiFillLike className="text-gray-400" />
               </div>
+
+              <p className="pl-[9px] font-thin text-[14px] text-gray-300">
+                5 Like
+              </p>
             </div>
           </div>
         </div>
--- src/page/CommunityPage.jsx
+++ src/page/CommunityPage.jsx
@@ -1,35 +1,49 @@
 // import { Navbar } from '../components/Navbar';
 
 import { Footer } from '../components/Footer';
-import { CommentContain } from '../components/items/Comment';
+import { CommentContent } from '../components/items/Comment';
 
 export const Community = () => {
   return (
     <>
-      <div className="mt-5 mb-2">
-        <h1 className="px-14 mb-3 font-bold">Discussion</h1>
+      <div className="flex">
+        <div className="flex-3">
+          <div className="mt-5 mb-2 ">
+            <h1 className="px-14 mb-3 font-bold">Discussion</h1>
 
-        {/* new discus */}
-        <div className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[827px] h-[72px] flex justify-between items-center">
-          <img
-            src="http://placehold.it/47x47"
-            alt="profile-img"
-            className="rounded-full ml-8 mr-[-450px]"
-          />
+            {/* new discus */}
+            <div className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[827px] h-[72px] flex justify-between items-center">
+              <img
+                src="http://placehold.it/47x47"
+                alt="profile-img"
+                className="rounded-full ml-8 mr-[-450px]"
+              />
 
-          <h4 className="text-center text-xl text-neutral-400 ">
-            Mulai diskusi baru
-          </h4>
+              <h4 className="text-center text-xl text-neutral-400 ">
+                Mulai diskusi baru
+              </h4>
 
-          <button className=" bg-[#FFCC81] w-[42px] h-[42px] rounded-[5px] mr-8 text-[30px]">
-            +
-          </button>
+              <button className=" bg-[#FFCC81] w-[42px] h-[42px] rounded-[5px] mr-8 text-[30px]">
+                +
+              </button>
+            </div>
+          </div>
+
+          <CommentContent />
+          <CommentContent />
+          <CommentContent />
+        </div>
+
+        <div className="flex-1">
+          <div className="1 mt-[55px] ml-[39px] mr-5 h-[573px] w-[374px] border border-[#ECECEC] bg-white flex flex-col justify-between items-center">
+            <div className="mt-auto mb-5">
+              <p className="text-center text-sm text-[#979797]">
+                advertisement
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      <CommentContain />
-      <CommentContain />
-      <CommentContain />
 
       {/* footer */}
       <div className="w-full relative">
