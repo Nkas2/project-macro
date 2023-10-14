5db06fd31a26bb6301bb28bbcf9ed67473bacb73
Rifky <rifkyferdiansyah211@gmail.com>
2023-10-14 23:00:52 +0700

    update community page

--- src/components/items/Comment.jsx
+++ src/components/items/Comment.jsx
@@ -1,45 +1,88 @@
 import { AiFillLike } from 'react-icons/ai';
+import PropTypes from 'prop-types';
 
-export const CommentContent = () => {
+export const CommentContent = ({
+  comment,
+  account,
+  uploadTime,
+  image,
+  like,
+  profileImage,
+}) => {
   return (
     <>
       {/* foto profile */}
-      <div className="my-3 flex-1">
-        <div className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[834px] h-[191px] flex justify-between items-center">
+      <div className="my-3 flex-4">
+        <div className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[834px] flex justify-between items-start">
           <img
-            src="http://placehold.it/47x47"
+            src={profileImage}
             alt="profile-img"
-            className="rounded-full ml-8 mb-[100px]"
+            className="rounded-full ml-8 mt-8 w-[47px] h-[47px]"
           />
 
-          {/* kolom komen */}
-          <div className="mx-8 flex-col">
-            <div className="comment pt-[35px]">
-              <h3 className="font-bold text-[20px] text-justify">
-                Halo, Guys aku mau tanya terkait perawatan kucing yang baik itu
-                gimana ya. Aku baru aja adopsi kucing
-              </h3>
-            </div>
+          <div className="flex-1 w-full">
+            {/* kolom komen */}
 
-            <div className="flex items-center space-x-3 pt-[5px] pb-[18px]">
-              <p className="pt-[px] text-[14px] text-[#FFCC81] font-bold">
-                Iqbal Arieftian
-              </p>
-              <p className="font-thin text-[14px] text-gray-400">Baru Saja</p>
-            </div>
-
-            <div className="pt-[5px] pb-[40px] flex items-center">
-              <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#D9D9D9] bg-opacity-[42%] rounded-full">
-                <AiFillLike className="text-gray-400" />
+            <div className="mx-8 flex-col">
+              <div className="comment pt-[25px]">
+                <h3 className="font-bold text-[20px] text-justify mt-3">
+                  {comment}
+                </h3>
+                {image && (
+                  <img
+                    src={image}
+                    alt=""
+                    className="mt-3 object-cover w-full h-[200px] rounded-[20px]"
+                  />
+                )}
               </div>
 
-              <p className="pl-[9px] font-thin text-[14px] text-gray-300">
-                5 Like
-              </p>
+              <div className="flex items-center space-x-3 pt-[5px] pb-[18px]">
+                <p className="pt-[px] text-[14px] text-[#FFCC81] font-bold">
+                  {/* Iqbal Arieftian */}
+                  {account}
+                </p>
+                <p className="font-thin text-[14px] text-gray-400">
+                  {/* Baru Saja */}
+                  {uploadTime}
+                </p>
+              </div>
+
+              <div className="pt-[5px] pb-[40px] flex items-center w-full">
+                <div className="flex items-center">
+                  <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#D9D9D9] bg-opacity-[42%] rounded-full cursor-pointer">
+                    <AiFillLike className="text-gray-400" />
+                  </div>
+
+                  <p className="pl-[9px] font-thin text-[14px] text-gray-300 ">
+                    {like}
+                  </p>
+                </div>
+
+                <div className="flex-1 justify-end">
+                  <div className="flex justify-end">
+                    <div className="bg-[#EFEDEC] w-[116px] h-[41px] flex items-center justify-center rounded-[15px] cursor-pointer">
+                      <img src="assets/image/icon-comment.svg" alt="" />
+                      <p className="pl-[10px] font-bold text-[14px] text-[#979797]">
+                        Jawab
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </>
   );
+};
+
+CommentContent.propTypes = {
+  comment: PropTypes.string.isRequired,
+  account: PropTypes.string.isRequired,
+  uploadTime: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  like: PropTypes.string.isRequired,
+  profileImage: PropTypes.string.isRequired,
 };
--- src/page/CommunityPage.jsx
+++ src/page/CommunityPage.jsx
@@ -1,7 +1,7 @@
 // import { Navbar } from '../components/Navbar';
 
-import { Footer } from "../components/Footer";
-import { CommentContent } from "../components/items/Comment";
+import { Footer } from '../components/Footer';
+import { CommentContent } from '../components/items/Comment';
 
 export const Community = () => {
   return (
@@ -29,9 +29,28 @@ export const Community = () => {
             </div>
           </div>
 
-          <CommentContent />
-          <CommentContent />
-          <CommentContent />
+          <CommentContent
+            profileImage="assets\image\profileImage\profile 1.png"
+            comment="Halo, Guys aku mau tanya terkait perawatan kucing yang baik itu gimana ya. Aku baru aja adopsi kucing"
+            account="Iqbal"
+            uploadTime="Baru Saja"
+            like="4 Like"
+          />
+          <CommentContent
+            profileImage="assets\image\profileImage\profile 2.png"
+            comment="Bagimana cara memandikan kucing yang benar ?"
+            account="Arya"
+            uploadTime="10 Menit lalu"
+            like="5 Like"
+          />
+          <CommentContent
+            profileImage="assets\image\profileImage\profile 3.png"
+            comment="Saya baru saja kehilangan kucing."
+            image="assets/image/commentImg.png"
+            account="Tyler"
+            uploadTime="20 Menit lalu"
+            like="20 Like"
+          />
         </div>
 
         <div className="flex-1">
