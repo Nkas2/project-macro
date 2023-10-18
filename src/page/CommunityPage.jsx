24e399b2711dd069d2e12097a693b6ba26e11e64
Rifky <rifkyferdiansyah211@gmail.com>
2023-10-18 23:12:06 +0700

    menambahkan reply pada komen dan membuat modals

--- src/page/CommunityPage.jsx
+++ src/page/CommunityPage.jsx
@@ -2,8 +2,19 @@
 import { Footer } from '../components/Footer';
 import { CommentContent } from '../components/items/Comment';
 import { comments } from '../data/comments';
+import { useState } from 'react';
 
 export const Community = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <>
       <div className="flex">
@@ -12,7 +23,9 @@ export const Community = () => {
             <h1 className="px-14 mb-3 font-bold">Discussion</h1>
 
             {/* new discus */}
-            <div className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[827px] h-[72px] flex justify-between items-center">
+            <div
+              onClick={openPopup}
+              className="bg-[#FFFDFC] rounded-[20px] border ml-[50px] w-[827px] h-[72px] flex justify-between items-center cursor-pointer">
               <img
                 src="http://placehold.it/47x47"
                 alt="profile-img"
@@ -27,6 +40,79 @@ export const Community = () => {
                 +
               </button>
             </div>
+
+            {/* Pop-up */}
+            {isPopupOpen && (
+              <div
+                className="relative z-10"
+                aria-labelledby="modal-title"
+                role="dialog"
+                aria-modal="true">
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                  <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="relative transform w-full h-full overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
+                      <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div className="sm:flex sm:items-start">
+                          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <img
+                              className="rounded-full"
+                              src="http://placehold.it/47x47"
+                              alt=""
+                            />
+                          </div>
+                          <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                            <h3
+                              className="text-base font-semibold leading-6 text-[#FFCC81]"
+                              id="modal-title">
+                              You
+                            </h3>
+                            <div className="mt-2">
+                              <textarea
+                                placeholder="Katakan Apapun"
+                                className="text-sm text-gray-500 w-96 h-20 resize-none focus:outline-none"></textarea>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 justify-between">
+                        <button
+                          onClick={closePopup}
+                          type="button"
+                          className="inline-flex w-full justify-center rounded-md bg-[#FFCC81] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#FFCC81] sm:ml-3 sm:w-auto">
+                          Post
+                        </button>
+                        <div>
+                          <img
+                            src="assets/image/icon-galery.png"
+                            alt=""
+                            className="cursor-pointer"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            {/* <div className="absolute flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-8 rounded">
+              <h2 className="text-xl font-bold mb-4">Tambah Komentar</h2>
+              <input
+                type="text"
+                placeholder="Tulis komentar di sini..."
+                className="w-full border border-gray-300 p-2 mb-4"
+              />
+              <div className="flex justify-end">
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded"
+                  onClick={closePopup}>
+                  Kirim
+                </button>
+              </div>
+            </div>
+          </div> */}
           </div>
           {comments.map((comment, index) => {
             return <CommentContent key={index} {...comment} />;
